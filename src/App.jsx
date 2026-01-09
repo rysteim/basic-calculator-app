0b2b6089be0cfd5446dd2b1301a99bedbd964ebf
rysteim <k.torino.139446.tc@umindanao.edu.ph>
2026-01-09 22:15:56 +0800

    Created the header of the opening page which contains the title and the logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import calculatorLogo from "./assets/images/calculator-logo.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen min-w-screen flex justify-center items-center">
+        {/* Header Section */}
+        <div className="flex justify-center py-4 items-center">
+          <h1 className="text-4xl font-bold ">Calculator App</h1>
+          <img
+            src={calculatorLogo}
+            alt="Calculator Logo"
+            className="w-20 h-20 ml-4"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
