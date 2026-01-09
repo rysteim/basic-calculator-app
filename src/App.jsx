a87c49f3e61b21b8eb155d344eba4751481c7ef5
rysteim <k.torino.139446.tc@umindanao.edu.ph>
2026-01-09 22:24:47 +0800

    Added buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,36 @@ import calculatorLogo from "./assets/images/calculator-logo.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="min-w-screen min-h-screen flex justify-center items-start">
+      <div className="flex-col min-w-screen min-h-screen flex justify-center items-center">
         {/* Header Section */}
-        <div className="flex justify-center py-4 items-center">
+        <div className="flex justify-center py-4 items-start">
           <h1 className="text-4xl font-bold ">Calculator App</h1>
           <img
             src={calculatorLogo}
             alt="Calculator Logo"
             className="w-20 h-20 ml-4"
           />
+        </div>
+        {/* Body Section */}
+        <div className="grid grid-cols-4 grid-rows-4">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>0</button>
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+          <button>%</button>
+          <button>=</button>
         </div>
       </div>
     </>
