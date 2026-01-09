57e30e156829732f406f93f9f6d8ea71853a2e5e
rysteim <k.torino.139446.tc@umindanao.edu.ph>
2026-01-09 22:29:04 +0800

    Modified button placements and added more buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,26 @@ function App() {
           />
         </div>
         {/* Body Section */}
-        <div className="grid grid-cols-4 grid-rows-4">
+        <div className="grid grid-cols-4 grid-rows-4 gap-2">
+          <button>C</button>
+          <button>CE</button>
+          <button>X</button>
+          <button>/</button>
           <button>1</button>
           <button>2</button>
           <button>3</button>
+          <button>*</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
+          <button>-</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>0</button>
           <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>/</button>
-          <button>%</button>
+          <button>+/-</button>
+          <button>0</button>
+          <button>.</button>
           <button>=</button>
         </div>
       </div>
